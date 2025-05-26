de094e8908a9f97756995be1cccb406d2f14c615
Raphael Mota <rphmota@gmail.com>
2025-05-26 20:47:08 -0300

    feat: inject providers and imports to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,88 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infra/database/database.module';
+import { ProdutorRepository } from './infra/database/repositories/produtor.repository';
+import { ProdutorController } from './interfaces/http/controllers/produtor.controller';
+import { CreateProdutorUseCase } from './app/usecases/create-produtor.usecase';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdutorOrmEntity } from './infra/entities/produtor.orm-entity';
+import { UpdateProdutorUseCase } from './app/usecases/update-produtor.usecase';
+import { DeleteProdutorUseCase } from './app/usecases/delete-produtor.usecase';
+import { FindProdutorUseCase } from './app/usecases/find-produtor.usecase';
+import { RelatorioProdutorEstadoUseCase } from './app/usecases/relatorio-produtor-estado.usecase';
+import { RelatorioProducaoController } from './interfaces/http/controllers/relatorio-producao.controller';
+import { RelatorioCulturasRepository } from './infra/database/repositories/relatorio-cultura.repository';
+import { RelatorioTotalFazendasUseCase } from './app/usecases/relatorio-total-farms.usecase';
+import { RelatorioTotalHectaresUseCase } from './app/usecases/relatorio-total-hectares';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ProdutorController, RelatorioProducaoController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    DatabaseModule,
+    TypeOrmModule.forFeature([ProdutorOrmEntity]),
+  ],
+  providers: [
+    {
+      provide: 'PRODUTOR_REPOSITORY',
+      useClass: ProdutorRepository,
+    },
+    {
+      provide: 'RELATORIO_PRODUTOR_ESTADO_REPOSITORY',
+      useClass: RelatorioCulturasRepository,
+    },
+    {
+      provide: 'CREATE_PRODUTOR_USE_CASE',
+      useFactory: (produtorRepo) => {
+        return new CreateProdutorUseCase(produtorRepo);
+      },
+      inject: ['PRODUTOR_REPOSITORY'],
+    },
+    {
+      provide: 'UPDATE_PRODUTOR_USE_CASE',
+      useFactory: (produtorRepo) => {
+        return new UpdateProdutorUseCase(produtorRepo);
+      },
+      inject: ['PRODUTOR_REPOSITORY'],
+    },
+    {
+      provide: 'DELETE_PRODUTOR_USE_CASE',
+      useFactory: (produtorRepo) => {
+        return new DeleteProdutorUseCase(produtorRepo);
+      },
+      inject: ['PRODUTOR_REPOSITORY'],
+    },
+    {
+      provide: 'FIND_PRODUTOR_USE_CASE',
+      useFactory: (produtorRepo) => {
+        return new FindProdutorUseCase(produtorRepo);
+      },
+      inject: ['PRODUTOR_REPOSITORY'],
+    },
+    {
+      provide: 'RELATORIO_PRODUTOR_ESTADO_USE_CASE',
+      useFactory: (produtorRepo) => {
+        return new RelatorioProdutorEstadoUseCase(produtorRepo);
+      },
+      inject: ['RELATORIO_PRODUTOR_ESTADO_REPOSITORY'],
+    },
+    {
+      provide: 'RELATORIO_TOTAL_FAZENDAS_USE_CASE',
+      useFactory: (relatorioRepo) => {
+        return new RelatorioTotalFazendasUseCase(relatorioRepo);
+      },
+      inject: ['RELATORIO_PRODUTOR_ESTADO_REPOSITORY'],
+    },
+    {
+      provide: 'RELATORIO_TOTAL_HECTARES_USE_CASE',
+      useFactory: (relatorioRepo) => {
+        return new RelatorioTotalHectaresUseCase(relatorioRepo);
+      },
+      inject: ['RELATORIO_PRODUTOR_ESTADO_REPOSITORY'],
+    },
+  ],
 })
 export class AppModule {}
